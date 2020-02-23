3d3389fa5d6a199f969b264ef9370eebe87bb6d4
Burak Sürmen <busurmen@gmail.com>
2020-02-24 00:45:11 +0300

    Added new intances to home and modal

--- home/index.js
+++ home/index.js
@@ -1,37 +1,61 @@
-class Mentee {
+// Members class to identify mentees and mentors properties
+class Members {
     constructor(json) {
         this.title = json.title;
-        this.location = json.location;
-        this.description = json.description
-        this.tags = json.tags
+        this.description = json.description;
+        this.country = json.country;
+        this.city = json.city;
+        this.startHour = json.start_hour;
+        this.endHour = json.end_hour;
+        this.tags = json.tags;
+        this.socialIconLinks = json.social_links.map(link => new SocialLink(link))
+        this.user_email = json.user_email;
     }
 }
 
+
+
+class SocialLink {
+    constructor(link) {
+        this.type = link.type;
+        this.value = link.value;
+    }
+}
+
+
+// Firebase request class to fetch data for any members and map it
 class FireBaseRequest {
-    static getMentees() {
-        let docRef = db.collection("mentees");
-        return docRef.get().then(
-            docs => docs.docs.map(doc => new Mentee(doc.data()))
+    static getMembers(memberRef) {
+
+        return memberRef.get().then(
+            docs => docs.docs.map(doc =>
+                new Members(doc.data())
+            )
         )
     }
 }
 
-class MenteesView {
-    static menteesList = document.getElementById("menteesList");
+// MembersView class renders data comes from Firebase.
+// Return data has same view structure, only texts change
+class MembersView {
+    static membersList = document.getElementById("membersList");
 
-    static renderMentee(mentee) {
-        const tagList = mentee.tags.map(tag => `
+
+    static renderMembers(member) {
+        const tagList = member.tags.map(tag => `
         <li class="tag-item"><a href="">${tag}</a></li>
         `).join('')
-        MenteesView.menteesList.insertAdjacentHTML('beforeend', `
-            <li id="menteeItem" class="list-item">
+
+
+        MembersView.membersList.insertAdjacentHTML('beforeend', `
+            <li id="memberItem" class="list-item">
                 <div class="box-content red darken-1 z-depth-3">
                     <div class="row valign-wrapper conatinerMine">
                         <div class="col l10 m9 s12">
-                            <h5 id="title">${mentee.title}</h5>
+                            <h5 id="title">${member.title}</h5>
                             <div>
                                 <i class="material-icons iconMine white-text">location_on</i>
-                                <p id="location" class="location">${mentee.location}</p>
+                                <p id="location" class="location">${member.city}, ${member.country}</p>
                             </div>
                             <span class="tagWord">Tags</span>
                             <ul id="tagsList" class="tagsList">
@@ -48,47 +72,77 @@ class MenteesView {
         `)
     }
 
-    static render(mentees) {
-        mentees.forEach(mentee => {
-            MenteesView.renderMentee(mentee);
+    static render(members) {
+        members.forEach(member => {
+            MembersView.renderMembers(member);
         });
     }
 }
 
 function run() {
-    FireBaseRequest.getMentees()
-        .then(mentees => {
-            MenteesView.render(mentees);
+
+    const isMentee = localStorage.getItem("mentee");
+
+
+
+
+    // If user click mentee gets data from mentees collection, for mentors gets from mentors db collection.
+    if (isMentee == 'true') {
+        memberRef = db.collection("mentees");
+    } else {
+        memberRef = db.collection("mentors");
+    }
+
+    memberRef = db.collection("mentees");
+
+    // This shows rendered member in the modal card.
+    FireBaseRequest.getMembers(memberRef).
+        then(members => {
+            MembersView.render(members);
+
+
 
             let modal = document.querySelector('.modal')
             let infoButtons = document.getElementsByClassName('infoButton')
             for (let i = 0; i < infoButtons.length; i++) {
                 infoButtons[i].addEventListener('click', function showModal() {
-                    const tagList = mentees[i].tags.map(tag => `
+                    const tagList = members[i].tags.map(tag => `
                     <li class="tag-item"><a href="">${tag}</a></li>
                     `).join('')
+
+
+                    const socialLink = members[i].socialIconLinks.map(sLink => `
+                    <li class="tag-item"><a href="">${sLink}</a></li>
+                    `).join('')
+                  
+
                     modal.innerHTML = `
-                            <div class="modal-content">
-                                <h4>${mentees[i].title}</h4>
-                                <div>
-                                    <i class="material-icons iconMine red-text">location_on</i>
-                                    <p id="location" class="location">${mentees[i].location}</p>
-                                </div>
-                                <h5>Description</h5>
-                                <p>${mentees[i].description}</p>
-                                <span class="tagWord">Tags</span>
-                                <ul id="tagsList" class="tagsList">
-                                    ${tagList}
-                                </ul>
-                                <div>                              
-                                <img class="socialIcons"
-                                    src="https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg" alt="">
-                                </div>
-                                <div class="center-align">
-                                    <a class="waves-effect waves-light btn red white-text infoBtn btn-large center-align">let's work together (:</a>
-                                </div>
-                            </div>
-                      
+                             <div class="modal-content">
+                                  <h4>starting with JavaScript${members[i].title}</h4>
+                                  <div>
+                                  <i class="material-icons iconMine red-text">location_on</i>
+                                  <p id="location" class="location">${members[i].city}, ${members[i].country}</p>
+                                  </div>
+                                  <h5>Description</h5>
+                                  <p> ${members[i].description}</p>
+                                  <span class="tagWord">Tags</span>
+                                  <ul id="tagsList" class="tagsList">
+                                     ${tagList}
+                                  </ul>
+                                  <p><span class="tagWord">Avaliable Hours:</span>${members[i].startHour} - ${members[i].endHour} </p>
+                                  <div class="social">
+                                     <p class="tagWord">Social Links:</p>
+                                     <a href="${socialLink}"><img class="socialIcons"
+                                     src="https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg" alt=""> </a>
+                                     <a href="${socialLink}"> <img class="socialIcons"
+                                     src="https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg"
+                                     alt=""></a>
+                                    </div>
+                                    <div class="center-align">
+                                       <a class="waves-effect waves-light btn red white-text infoBtn btn-large center-align">let's work
+                                               together (:</a>
+                                    </div>
+                             </div>  
                     `
                     modalInstance.open()
                 })
