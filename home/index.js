230ba57340fa8f784ed0d0a95254cc48f225c2e9
osamaakb <osama98akb@gmail.com>
2020-02-29 13:25:10 +0300

    put renderModelInfo in a seperate function

--- home/index.js
+++ home/index.js
@@ -50,123 +50,126 @@ class MembersView {
     }
 
     static renderInfoModal(members) {
-        let modal = document.querySelector('.modal')
         let infoButtons = document.getElementsByClassName('infoButton')
 
         for (let i = 0; i < infoButtons.length; i++) {
-            infoButtons[i].addEventListener('click', function showModal() {
-                const tagList = members[i].tags.map(tag => `
-                <li class="tag-item"><a href="">${tag}</a></li>
-                `).join('')
-
-                modal.innerHTML = `
-                         <div class="modal-content">
-                              <h4>${members[i].title}<span id='iconSpan'><i class="iconKaan penIcon hidden material-icons">mode_edit</i><i class="iconKaan trashIcon hidden fas fa-trash"></i></span></h4>
-                              <div>
-                              <i class="material-icons iconMine red-text">location_on</i>
-                              <p id="location" class="location">${members[i].city}, ${members[i].country}</p>
-                              </div>
-                              <h5>Description</h5>
-                              <p> ${members[i].description}</p>
-                              <span class="tagWord">Tags</span>
-                              <ul id="tagsList" class="tagsList">
-                                 ${tagList}
-                              </ul>
-                              <p><span class="tagWord">Avaliable Hours: </span>${members[i].startHour} - ${members[i].endHour} </p>
-                              <div class="social">
-                                 <p class="tagWord">Social Links:</p>
-                                </div>
-                                <div  class="center-align">
-                                   <a id="work-btn" class="waves-effect waves-light btn red white-text infoBtn btn-large center-align">let's work
-                                           together (:</a>
-                                </div>
-                         </div>  `
-
-                let trash = document.querySelector('.trashIcon')
-
-                trash.addEventListener('click', function () {
-                    db.collection(memberType).doc(members[i].doc_id).delete()
-                        .then(function (docRef) {
-                            alert('Title: ' + members[i].title + ' DELETED')
-                            window.location = `./index.html?type=${memberType}`;
-                        })
-                        .catch(function (error) {
-                            console.error("Error adding document: ", error);
-                        });
-
-                })
-
-
-                let workBtn = document.querySelector('#work-btn')
-                workBtn.addEventListener('click', Auth.sendEmail)
-
-                let social = document.querySelector("#memberInfoModal > div > div.social")
-
-                const socialLinksObject = members[i].socialIconLinks.map(links => links);
-                const socialLinks = {}
-                // This part organizes social icons. if member has only github account, the only github icon is rendered on the modal.
-                socialLinksObject.forEach(socialIcon => {
-
-                    if (socialIcon.value != "") {
-                        if (socialIcon.type == "github") {
-                            socialLinks['github'] = socialIcon.value
-                            social.insertAdjacentHTML('beforeend', `<a href="${socialIcon.value}"><img class="socialIcons"
-                                     src="https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg" alt=""> </a>`);
-                        } if (socialIcon.type == "linkedin") {
-                            socialLinks['linkedin'] = (socialIcon.value)
-
-                            social.insertAdjacentHTML('beforeend', `<a href="${socialIcon.value}"><img class="socialIcons"
-                                     src="https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg" alt=""> </a>`);
-                        } if (socialIcon.type == "twitter") {
-                            socialLinks['twitter'] = (socialIcon.value)
-
-                            social.insertAdjacentHTML('beforeend', `<a href="${socialIcon.value}"><img class="socialIcons"
-                                     src="https://pngimage.net/wp-content/uploads/2018/06/official-twitter-logo-png-3.png" alt=""> </a>`
-                            )
-                        }
-                    }
-
-
-                })
-
-                let socialIconsVisibility = document.querySelectorAll("#memberInfoModal > div > div.social > a")
-
-                if (Auth.isLoggedIn == false) {
-                    socialIconsVisibility.forEach(hideIcon => hideIcon.remove())
-                }
-
-                social.insertAdjacentHTML('beforeend', `<button class="loginBtn"  onclick="signInModalInstance.open();" >Login </button>`);
-
-                let loginBtnVisibility = document.querySelector("#memberInfoModal > div > div.social > button")
-
-                if (Auth.isLoggedIn == true) {
-                    loginBtnVisibility.remove()
-                }
-                memberInfoModalInstance.open()
-
-                let icons = document.getElementsByClassName('iconKaan')
-                if (Auth.isLoggedIn) {
-                    let userEmail = JSON.parse(localStorage.getItem('firebase:authUser:AIzaSyAQmIYmYmq2OXM1zuJanex1paJpXJp3ZXc:[DEFAULT]')).email
-                    if (userEmail === members[i].user_email) {
-                        for (let index = 0; index < icons.length; index++) {
-                            icons[index].classList.remove('hidden')
-                        }
-                    }
-                }
-
-                let editIcon = icons[0]
-                const urlParams = new URLSearchParams(window.location.search);
-                const memberType = urlParams.get('type');
-
-                editIcon.addEventListener('click', () => {
-                    localStorage.setItem('changeForm', true);
-                    localStorage.setItem('user', JSON.stringify(members[i]))
-                    Auth.openFormModal(memberType)
-                })
-
-            })
+            infoButtons[i].addEventListener('click', () => MembersView.showModal(members[i]))
         }
     }
+
+    static showModal(member) {
+        let modal = document.querySelector('.modal')
+        const tagList = member.tags.map(tag => `
+        <li class="tag-item"><a href="">${tag}</a></li>
+        `).join('')
+
+        modal.innerHTML = `
+                 <div class="modal-content">
+                      <h4>${member.title}<span id='iconSpan'><i class="iconKaan penIcon hidden material-icons">mode_edit</i><i class="iconKaan trashIcon hidden fas fa-trash"></i></span></h4>
+                      <div>
+                      <i class="material-icons iconMine red-text">location_on</i>
+                      <p id="location" class="location">${member.city}, ${member.country}</p>
+                      </div>
+                      <h5>Description</h5>
+                      <p> ${member.description}</p>
+                      <span class="tagWord">Tags</span>
+                      <ul id="tagsList" class="tagsList">
+                         ${tagList}
+                      </ul>
+                      <p><span class="tagWord">Avaliable Hours: </span>${member.startHour} - ${member.endHour} </p>
+                      <div class="social">
+                         <p class="tagWord">Social Links:</p>
+                        </div>
+                        <div  class="center-align">
+                           <a id="work-btn" class="waves-effect waves-light btn red white-text infoBtn btn-large center-align">let's work
+                                   together (:</a>
+                        </div>
+                 </div>  `
+
+        let trash = document.querySelector('.trashIcon')
+
+        trash.addEventListener('click', function () {
+            db.collection(memberType).doc(member.doc_id).delete()
+                .then(function (docRef) {
+                    alert('Title: ' + member.title + ' DELETED')
+                    window.location = `./index.html?type=${memberType}`;
+                })
+                .catch(function (error) {
+                    console.error("Error adding document: ", error);
+                });
+
+        })
+
+
+        let workBtn = document.querySelector('#work-btn')
+        workBtn.addEventListener('click', Auth.sendEmail)
+
+        let social = document.querySelector("#memberInfoModal > div > div.social")
+
+        const socialLinksObject = member.socialIconLinks.map(links => links);
+        const socialLinks = {}
+        // This part organizes social icons. if member has only github account, the only github icon is rendered on the modal.
+        socialLinksObject.forEach(socialIcon => {
+
+            if (socialIcon.value != "") {
+                if (socialIcon.type == "github") {
+                    socialLinks['github'] = socialIcon.value
+                    social.insertAdjacentHTML('beforeend', `<a href="${socialIcon.value}"><img class="socialIcons"
+                             src="https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg" alt=""> </a>`);
+                } if (socialIcon.type == "linkedin") {
+                    socialLinks['linkedin'] = (socialIcon.value)
+
+                    social.insertAdjacentHTML('beforeend', `<a href="${socialIcon.value}"><img class="socialIcons"
+                             src="https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg" alt=""> </a>`);
+                } if (socialIcon.type == "twitter") {
+                    socialLinks['twitter'] = (socialIcon.value)
+
+                    social.insertAdjacentHTML('beforeend', `<a href="${socialIcon.value}"><img class="socialIcons"
+                             src="https://pngimage.net/wp-content/uploads/2018/06/official-twitter-logo-png-3.png" alt=""> </a>`
+                    )
+                }
+            }
+
+
+        })
+
+        let socialIconsVisibility = document.querySelectorAll("#memberInfoModal > div > div.social > a")
+
+        if (Auth.isLoggedIn == false) {
+            socialIconsVisibility.forEach(hideIcon => hideIcon.remove())
+        }
+
+        social.insertAdjacentHTML('beforeend', `<button class="loginBtn"  onclick="signInModalInstance.open();" >Login </button>`);
+
+        let loginBtnVisibility = document.querySelector("#memberInfoModal > div > div.social > button")
+
+        if (Auth.isLoggedIn == true) {
+            loginBtnVisibility.remove()
+        }
+        memberInfoModalInstance.open()
+
+        let icons = document.getElementsByClassName('iconKaan')
+        if (Auth.isLoggedIn) {
+            let userEmail = JSON.parse(localStorage.getItem('firebase:authUser:AIzaSyAQmIYmYmq2OXM1zuJanex1paJpXJp3ZXc:[DEFAULT]')).email
+            if (userEmail === member.user_email) {
+                for (let index = 0; index < icons.length; index++) {
+                    icons[index].classList.remove('hidden')
+                }
+            }
+        }
+
+        let editIcon = icons[0]
+        const urlParams = new URLSearchParams(window.location.search);
+        const memberType = urlParams.get('type');
+
+        editIcon.addEventListener('click', () => {
+            localStorage.setItem('changeForm', true);
+            localStorage.setItem('user', JSON.stringify(member))
+            Auth.openFormModal(memberType)
+        })
+
+    }
+
 }
 
 async function run() {
@@ -176,8 +179,6 @@ async function run() {
 
     FireBaseRequest.getMembers(memberRef).
         then(members => {
-            console.log(members);
-
             MembersView.render(members);
             MembersView.renderInfoModal(members)
         })
@@ -193,6 +194,7 @@ function configureNavButtons() {
 
     const urlParams = new URLSearchParams(window.location.search);
     const memberType = urlParams.get('type');
+
 
     // If user click mentee gets data from mentees collection, for mentors gets from mentors db collection.
     if (memberType == 'mentees') {
@@ -219,6 +221,8 @@ function configureNavButtons() {
     showSignOutBtn.forEach(btn =>
         btn.addEventListener('click', Auth.signOut))
 
-}
+    // isMemberBefore(memberType)
 
+
+}
 document.addEventListener("DOMContentLoaded", run);
